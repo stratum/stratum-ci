3dae95ca061f9a21d7daa0d8ae52d9e0ea39538b
Abhilash Endurthi <abhilash@opennetworking.org>
2020-04-20 12:31:02 -0700

    Push correct docker image to registry

--- Jenkinsfiles/Jenkinsfile-stratum-bf-build.groovy
+++ Jenkinsfiles/Jenkinsfile-stratum-bf-build.groovy
@@ -19,19 +19,19 @@ pipeline {
 		stage('Build') {
 			steps {
 				step([$class: 'WsCleanup'])
-				sh returnStdout: false, label: "Start building stratum-bf:${SDE_VERSION}-${KERNEL_VERSION}-OpenNetworkLinux", script: """
+				sh returnStdout: false, label: "Start building stratum-bf:bf-sde-${SDE_VERSION}-linux-${KERNEL_VERSION}-OpenNetworkLinux", script: """
 					git clone https://github.com/stratum/stratum.git
 					cd ${WORKSPACE}/stratum
 					cd ${WORKSPACE}/stratum/stratum/hal/bin/barefoot/docker
 					./build-stratum-bf-container.sh /var/jenkins/stratum-contents/bf-sde-${SDE_VERSION}.tgz /var/jenkins/stratum-contents/linux-${KERNEL_VERSION}-OpenNetworkLinux.tar.xz
-					docker tag stratumproject/stratum-bf:bf-sde-${SDE_VERSION}-linux-${KERNEL_VERSION}-OpenNetworkLinux ${DOCKER_REGISTRY_IP}:${DOCKER_REGISTRY_PORT}/stratum-bf:${SDE_VERSION}-${KERNEL_VERSION}-OpenNetworkLinux
-					docker push ${DOCKER_REGISTRY_IP}:${DOCKER_REGISTRY_PORT}/stratum-bf:${SDE_VERSION}-${KERNEL_VERSION}-OpenNetworkLinux
+					docker tag stratumproject/stratum-bf:bf-sde-${SDE_VERSION}-linux-${KERNEL_VERSION}-OpenNetworkLinux ${DOCKER_REGISTRY_IP}:${DOCKER_REGISTRY_PORT}/stratum-bf:bf-sde-${SDE_VERSION}-linux-${KERNEL_VERSION}-OpenNetworkLinux
+					docker push ${DOCKER_REGISTRY_IP}:${DOCKER_REGISTRY_PORT}/stratum-bf:bf-sde-${SDE_VERSION}-linux-${KERNEL_VERSION}-OpenNetworkLinux
 				"""
 			}
 		}
 		stage('Unit Test') {
 			steps {
-				sh returnStdout: false, label: "Run unit tests for stratum-bf:${SDE_VERSION}-${KERNEL_VERSION}-OpenNetworkLinux", script: """
+				sh returnStdout: false, label: "Run unit tests for stratum-bf:bf-sde-${SDE_VERSION}-linux-${KERNEL_VERSION}-OpenNetworkLinux", script: """
 					cd ${WORKSPACE}/stratum/
 					sed -i '1i build --disk_cache=/tmp/bazel-disk-cache' .bazelrc
 					docker run --rm -v ${BAZEL_DISK_CACHE}:/tmp/bazel-disk-cache -v ${WORKSPACE}/stratum:/stratum ${DOCKER_REGISTRY_IP}:${DOCKER_REGISTRY_PORT}/stratum-unit
