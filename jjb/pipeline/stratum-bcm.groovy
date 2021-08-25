fabc65e6a7408855542ef32d24318674911fee1b
Siddesh Sreenivasan <siddesh.sreeni@gmail.com>
2021-08-24 17:12:14 -0700

    changes in stratum bcm pushing into Dockerhub

--- jjb/pipeline/stratum-bcm.groovy
+++ jjb/pipeline/stratum-bcm.groovy
@@ -3,68 +3,46 @@ pipeline {
         label "${BUILD_NODE}"
     }
     options {
-        timeout(time: 90, unit: 'MINUTES')
+        timeout(time: 60, unit: 'MINUTES')
     }
     stages {
-        stage('Build, Test and Publish') {
-            matrix {
-                axes {
-                    axis {
-                        name 'KERNEL_VERSION'
-                        values '4.14.49'
-                    }
-                    axis {
-                        name 'SDE'
-                        values 'sdklt', 'opennsa'
-                    }
+        stage('Build Debian Package') {
+            steps {
+                step([$class: 'WsCleanup'])
+                sh returnStdout: false, label: "Start building stratum-bcm_${SDE}_deb", script: """
+                    git clone https://github.com/stratum/stratum.git
+                    cd ${WORKSPACE}/stratum
+                    sed -i 's/ -ti/ --tty/g' setup_dev_env.sh
+                    ./setup_dev_env.sh -- --name stratum & 
+                    sleep 120
+                    docker exec -t stratum bazel build //stratum/hal/bin/bcm/standalone:stratum_bcm_${SDE}_deb
+                """
+            }
+        }
+        stage('Save Debian Package') {
+            steps {
+                withAWS(credentials:"${AWS_S3_CREDENTIAL}") {
+                    s3Upload(file:"${WORKSPACE}/stratum/bazel-bin/stratum/hal/bin/bcm/standalone/stratum_bcm_${SDE}_deb.deb", bucket:'stratum-artifacts', path:"stratum_bcm_${SDE}_deb.deb")
                 }
-                agent {
-                    label "${BUILD_NODE}"
-                }
-                stages {
-                    stage("Build") {
-                        steps {
-                            sh returnStdout: false, label: "Start building stratum-bcm:${SDE}", script: ""
-                            build job: "stratum-bcm-build", parameters: [
-				                string(name: 'KERNEL_VERSION', value: "${KERNEL_VERSION}"),
-                                string(name: 'SDE', value: "${SDE}"),
-                                string(name: 'REGISTRY_URL', value: "${REGISTRY_URL}"),
-                                string(name: 'REGISTRY_CREDENTIAL', value: "${REGISTRY_CREDENTIAL}"),
-                            ]
-                        }
-                    }
-                    stage('Test') {
-                        steps {
-                            sh returnStdout: false, label: "Start testing ${REGISTRY_URL}/stratum-bcm:${SDE}", script: ""
-                            build job: "stratum-bcm-test-combined", parameters: [
-                                string(name: 'REGISTRY_URL', value: "${REGISTRY_URL}"),
-                                string(name: 'REGISTRY_CREDENTIAL', value: "${REGISTRY_CREDENTIAL}"),
-                                string(name: 'AWS_S3_CREDENTIAL', value: "${AWS_S3_CREDENTIAL}"),
-                                string(name: 'DOCKER_IMAGE', value: "stratum-bcm"),
-                                string(name: 'DOCKER_IMAGE_TAG', value: "${SDE}"),
-                                string(name: 'DEBIAN_PACKAGE_PATH', value: "path"),
-                                string(name: 'DEBIAN_PACKAGE_NAME', value: "stratum_bcm_${SDE}_deb.deb"),
-                            ]
-                        }
-                    }
-                    stage('Publish') {
-                        steps {
-                            sh returnStdout: false, label: "Start publishing ${REGISTRY_URL}/stratum-bcm:${SDE}", script: ""
-                            build job: "stratum-publish", parameters: [
-                                string(name: 'REGISTRY_URL', value: "${REGISTRY_URL}"),
-                                string(name: 'REGISTRY_CREDENTIAL', value: "${REGISTRY_CREDENTIAL}"),
-                                string(name: 'DOCKER_REPOSITORY_NAME', value: "stratum-bcm"),
-                                string(name: 'DOCKER_IMAGE_TAG', value: "${SDE}"),
-                            ]
-                        }
-                    }
+            }
+        }
+        stage('Build Docker Image') {
+            steps {
+                sh returnStdout: false, label: "Start building stratum-bcm:${SDE}", script: """
+                    cp ${WORKSPACE}/stratum/bazel-bin/stratum/hal/bin/bcm/standalone/stratum_bcm_${SDE}_deb.deb ${WORKSPACE}/stratum/stratum/hal/bin/bcm/standalone/docker/stratum_bcm_deb.deb
+                    cd ${WORKSPACE}/stratum/stratum/hal/bin/bcm/standalone/docker
+                    docker build -t stratumproject/stratum-bcm:latest-${SDE} .
+                """
+            }
+        }
+        stage('Push'){
+            steps {
+                withDockerRegistry([ credentialsId: "${ONF_DOCKER_HUB_CREDENTIAL}", url: "" ]) {
+                    sh returnStdout: false, label: "Start publishing stratumproject/stratum-bcm:latest-${SDE}", script: """
+                        docker push stratumproject/stratum-bcm:latest-${SDE}
+		            """   
                 }
             }
         }
     }
-    /*post {
-        failure {
-             slackSend color: 'danger', message: "Test failed: ${env.JOB_NAME} #${env.BUILD_NUMBER} (<${env.RUN_DISPLAY_URL}|Open>)"
-        }
-    }*/
 }
